5765d8ca9d344ba5902eaa5bdf4485be65cb4892
man53d <mman35943@gmail.com>
2025-07-19 01:15:49 +0300

    Add setup.py to make it pip-installable

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='seed_generator',
+    version='0.1',
+    py_modules=['seed_generator'],
+    author='man53d',
+    author_email='mman35943@gmail.com',
+    description='Generate a strong random seed from multiple sources',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
+ 
